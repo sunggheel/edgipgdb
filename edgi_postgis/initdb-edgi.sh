cc6f4eb92c42cfe7be1af277a2f233fe24539de4
sunggheel <sunggheel@gmail.com>
2022-09-19 19:42:23 -0400

    update

--- edgi_postgis/initdb-edgi.sh
+++ edgi_postgis/initdb-edgi.sh
@@ -5,8 +5,7 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
-# Create the 'template_postgis' template db
-echo "Creating the slim_echoepa database....."
+# Create the 'slim_echoepa' database
 "${psql[@]}" <<- 'EOSQL'
 CREATE USER echoepa WITH ENCRYPTED PASSWORD 'echoepa';
 CREATE USER echoepa_public WITH ENCRYPTED PASSWORD 'echoepa_public';
@@ -21,10 +20,12 @@ GRANT USAGE ON SCHEMA public TO echoepa_public;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO echoepa_public;
 EOSQL
 
-echo "Creating schemas for the slim_echoepa database....."
+# Create schemas for the slim_echoepa database
 psql -d slim_echoepa -f /echo_scripts/schema.sql
 
-echo "installing python3 modules for echo_scripts....."
+# Install python3 modules for echo_scripts
 pip install psycopg2-binary
 pip install sqlalchemy
 pip install csvkit
+
+cp -R /echo_scripts /var/lib/posgresql/data
